17de66b3e4b2bbfec6b65125cf2d09821e3cdbba
edriophthalma <86781480+edriophthalma@users.noreply.github.com>
2021-11-11 12:42:26 +0000

    added bootstrap header and footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="cotainer">
+        <header>Weather forecast</header>
+       <footer><a href="https://github.com/edriophthalma/weather-react.git" alt="Giulia's code">Open-source code by Giulia D'Angelo</a></footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
