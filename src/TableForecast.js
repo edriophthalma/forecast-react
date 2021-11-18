62f52a34a9e16b9f11c2f041d1a924fadd89a2a8
edriophthalma <86781480+edriophthalma@users.noreply.github.com>
2021-11-18 14:56:48 +0000

    Update TableForecast.js

--- src/TableForecast.js
+++ src/TableForecast.js
@@ -18,31 +18,31 @@ export default function TableForecast(props) {
  setForecast(response.data.daily); 
  goLoaded(true);
 }
-
-
-    if(loaded) {
-       return (
-           
-           <div className="row">
-  {forecast.map(function(dailyForecast, index) { if (index >= 1 && index <= 5) {
-                    return (<div className="col" key={index}><Days data={dailyForecast} />
-               </div>);}}
-   )} </div>);
-       
-       
-              
-                }
-       
- else 
-    
-    {
-
-    let apiKey = "2d50c0d7967e795bde908aa93c3e908d";
+function load() {
+     let apiKey = "2d50c0d7967e795bde908aa93c3e908d";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
 
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(DailyForecast);
+}
+
+
+    if(loaded) {
+       return (
+           <div className="days">
+           <div className="row">
+  {forecast.map(function(dailyForecast, index) { if (index >= 1 && index <= 5) {
+                    return (<div className="col" key={index}><Days data={dailyForecast} />
+               </div>)} else{ return null;}}
+   )} </div>)</div>);
+        }
+       
+ else 
+    
+    {
+
+   load();
         
     return null;
 }
