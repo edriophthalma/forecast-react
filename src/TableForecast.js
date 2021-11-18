f7a42d24bf57514bcf8fd4e876def19f20ba262b
edriophthalma <86781480+edriophthalma@users.noreply.github.com>
2021-11-18 14:34:15 +0000

    css fixing

--- src/TableForecast.js
+++ src/TableForecast.js
@@ -18,22 +18,16 @@ export default function TableForecast(props) {
  setForecast(response.data.daily); 
  goLoaded(true);
 }
-function load() {
-    let apiKey = "2d50c0d7967e795bde908aa93c3e908d";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(DailyForecast);
-}
 
     if(loaded) {
        return (
+           
            <div className="row">
   {forecast.map(function(dailyForecast, index) { if (index >= 1 && index <= 5) {
                     return (<div className="col" key={index}><Days data={dailyForecast} />
                </div>);}}
-   )} </div>)
+   )} </div>);
        
        
               
@@ -41,7 +35,16 @@ function load() {
        
  else 
     
-    load(); {
-    return null}
+    {
+
+    let apiKey = "2d50c0d7967e795bde908aa93c3e908d";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(DailyForecast);
+        
+    return null;
+}
    
 }
